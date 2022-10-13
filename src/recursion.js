9a58755a311dc9b769e43a38d1375f6a9361aef1
Maxwell Peterson <mpeterson94@me.com>
2022-10-13 09:32:03 -0700

    progress

--- src/recursion.js
+++ src/recursion.js
@@ -40,17 +40,35 @@ var arraySum = function(array) {
 
 // 4. Check if a number is even.
 var isEven = function(n) {
+  if (n === 0) {
+    return true;
+  } else if (n === 1) {
+    return false;
+  } if (n > 1) {
+    return isEven(n - 2);
+  } if (n < 0) {
+    return isEven(n + 2);
+  }
 };
 
 // 5. Sum all integers below a given integer.
 // sumBelow(10); // 45
 // sumBelow(7); // 21
 var sumBelow = function(n) {
+  // if (!sum_sumBelow) var sum_sumBelow = 0;
+  if (n > 0) {
+    return n - 1 + sumBelow(n - 1);
+  } else if (n < 0) {
+    return n + 1 + sumBelow(n + 1)
+  } else {
+    return 0;
+  }
 };
 
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
 var range = function(x, y) {
+
 };
 
 // 7. Compute the exponent of a number.
